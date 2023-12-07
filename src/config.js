2a5dbc63797422bcda86e679e92a31f239c3000e
joaojmag <joaojmag@gmail.com>
2023-12-07 00:23:18 -0300

    atualizando o src/config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Arquivo de configuração para a URL da API
 
 // Define a URL da API que será usada no projeto. Certifique-se de substituir esta URL pela URL real da sua API.
-const API_URL = "http://localhost:8080/api";//"https://650aef11dfd73d1fab0937b6.mockapi.io/api/";
+const API_URL = "https://back-end-lista-atividades.onrender.com/api";
 
 // Exporta a URL da API como padrão, para que ela possa ser importada em outros arquivos.
 export default API_URL;
